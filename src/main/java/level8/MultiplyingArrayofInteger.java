2c57024e239a8b9a16d9aaf92b5172b56a2d1499
Ruslan Murmylo <ruslanmurmilo@gmail.com>
2023-04-03 15:44:40 +0300

    finish task4

---
+++ src/main/java/level8/MultiplyingArrayofInteger.java
@@ -0,0 +1,13 @@
+package level8;
+
+public class MultiplyingArrayofInteger {
+
+    public static int grow(int[] x) {
+        int result = 1;
+        for (int i = 0; i < x.length; i++) {
+            result *= x[i];
+        }
+        return result;
+
+    }
+}
