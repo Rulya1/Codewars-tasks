d5527e01cb2815ce83abd2f8f195323b47da4590
Ruslan Murmylo <ruslanmurmilo@gmail.com>
2023-04-03 15:02:44 +0300

    finish task3

--- src/main/java/level8/DoesArrayContainElement.java
+++ src/main/java/level8/DoesArrayContainElement.java
@@ -1,7 +1,11 @@
 package level8;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DoesArrayContainElement {
     public static boolean check(Object[] a, Object x) {
+        List<Object> list = List.of(a);
         for (Object obj : a) {
             if (obj.equals(x))
             {
---
+++ src/main/java/level8/EvenorOddNumber.java
@@ -0,0 +1,12 @@
+package level8;
+
+public class EvenorOddNumber {
+    public static String even_or_odd(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+}
+
