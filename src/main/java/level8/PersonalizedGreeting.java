6b0237ec56d16cac5f7891ca28a412cd37d3fd44
Ruslan Murmylo <ruslanmurmilo@gmail.com>
2023-04-03 14:06:43 +0300

    inital commit

---
+++ src/main/java/level8/PersonalizedGreeting.java
@@ -0,0 +1,11 @@
+package level8;
+
+public class PersonalizedGreeting {
+    static String greet(String name, String owner) {
+        if (name.equals(owner)) {
+            return "Hello boss";
+        } else {
+            return "Hello guest";
+        }
+    }
+}
